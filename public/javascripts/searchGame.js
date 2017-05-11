11ca4648f922b631d679c2574a50c527a5cf8ae4
SamuelioX <samueliox@gmail.com>
2017-05-10 20:05:00 -0700

    updated search function to use ajax

--- public/javascripts/searchGame.js
+++ public/javascripts/searchGame.js
@@ -2,14 +2,10 @@ var searchApp = angular.module("searchApp", []);
 searchApp.controller("searchCtrl", function ($scope, $http) {
     var name = $scope.gamename;
 
-    $scope.findValue = function (name) {
+    $scope.searchGame = function (name) {
         var endpoint = "http://localhost:3000/searchGame?gamename=" + name;
         $http.get(endpoint).then(function (response) {
             $scope.response = response.data;
-//            $scope.myWelcome = response;
-//                        for(var i = 0; i < response.data.length; i++){
-//                            $scope.myWelcome += + response.data[i].email;
-//                        }
         });
     };
 });
