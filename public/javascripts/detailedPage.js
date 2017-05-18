905b784270659e493e3c40b1bce7f228de9b8d82
SamuelioX <samueliox@gmail.com>
2017-05-17 19:55:16 -0700

    forgot to add detailed page

--- public/javascripts/detailedPage.js
+++ public/javascripts/detailedPage.js
@@ -12,7 +12,7 @@ detailedApp.controller("detailedCtrl", function ($scope, $http, $location) {
     $scope.detailedPage = function () {
         var loc = $location.search();
         var id = $location.search().id;
-        var endpoint = "http://localhost:3000/getGameInfo?gameId=" + id;
+        var endpoint = "http://sample-env-1.hdvxynmgpf.us-west-2.elasticbeanstalk.com/getGameInfo?gameId=" + id;
         $http.get(endpoint).then(function (response) {
             $scope.response = response.data;
         });
